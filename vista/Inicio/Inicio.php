380e4db184ba9e2dd625b6921ed4ad5e13c06c6d
JoseAle2610 <jalesgervazzi@gmail.com>
2021-06-04 07:01:06 -0400

    Inicio - Card de informacion

--- vista/Inicio/Inicio.php
+++ vista/Inicio/Inicio.php
@@ -1,28 +1,65 @@
 <div class="row">
 	<div class="col-3 d-flex flex-column">
-		<div class="card mb-3">
-			<div class="card-header">
-				Alimentacion
+
+		<div class="card mb-3 bg-primary border-white shadow">
+			<div class="card-body py-0 pr-2">
+				<div class="row">
+					<div class="col-2 text-center d-flex justify-content-center">
+						<img src="https://image.flaticon.com/icons/png/128/3363/3363571.png" height="40px" class="align-self-center">
+					</div>
+					<div class="col-10 bg-white rounded">
+						<h5>Alimentacion
+							<label class="text-muted h6">fecha: <span>20-05-2021</span></label>
+						</h5>
+						<p class=" font-weight-bold">Consumo por Ave: <span class="text-primary">150g</span></p>
+						<p class=" font-weight-bold">Consumo Total: <span class="text-primary">150k</span></p>
+						<p class=" font-weight-bold">Alimento en inventario: <span class="text-primary">500k</span></p>
+
+					</div>
+				</div>
 			</div>
-			<div class="card-body">Alimentacion </div>
 		</div>
-		<div class="card mb-3">
-			<div class="card-header">
-				Mortalidad
+
+		<div class="card mb-3 bg-danger border-white shadow">
+			<div class="card-body py-0 pr-2">
+				<div class="row">
+					<div class="col-2 text-center d-flex justify-content-center">
+						<img src="https://image.flaticon.com/icons/png/128/1979/1979819.png" height="40px" class="align-self-center">
+					</div>
+					<div class="col-10 bg-white rounded">
+						<h5>Mortalidad
+							<label class="text-muted h6">fecha: <span>20-05-2021</span></label>
+						</h5>
+						<p class=" font-weight-bold">Mortalidad Esperada: <span class="text-danger">0.15%</span></p>
+						<p class=" font-weight-bold">Mortalidad Obtenida: <span class="text-danger">1%</span></p>
+						<p class=" font-weight-bold">Mortaloidad Total: <span class="text-danger">10 Gallinas</span></p>
+						<p class=" font-weight-bold">Gallinas Restantes: <span class="text-danger">1000</span></p>
+
+					</div>
+				</div>
 			</div>
-			<div class="card-body">Mortalidad </div>
 		</div>
-		<div class="card mb-3">
-			<div class="card-header">
-				Produccion
+
+		<div class="card mb-3 bg-info border-white shadow">
+			<div class="card-body py-0 pr-2">
+				<div class="row">
+					<div class="col-2 text-center d-flex justify-content-center">
+						<img src="assets/img/transportador.svg" height="40px" class="align-self-center">
+					</div>
+					<div class="col-10 bg-white rounded">
+						<h5>Produccion
+							<label class="text-muted h6">fecha: <span>20-05-2021</span></label>
+						</h5>
+						<p class=" font-weight-bold">Produccion Esperada: <span class="text-info">60%</span></p>
+						<p class=" font-weight-bold">Produccion Obtenida: <span class="text-info">61%</span></p>
+						<p class=" font-weight-bold">Huevos Producidos: <span class="text-info">2000</span></p>
+
+					</div>
+				</div>
 			</div>
-			<div class="card-body">Produccion </div>
 		</div>
-		<div class="card mb-3">
-			<div class="card-header">
-				Huevos
-			</div>
-			<div class="card-body">Huevos</div>
-		</div>
+
+
+		
 	</div>
 </div>
